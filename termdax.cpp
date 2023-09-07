857ef8aa9b1d295537b7c57356b3afec63c392eb
dahni <94286336+D1hn1@users.noreply.github.com>
2023-09-07 19:38:53 +0200

    Update termdax.cpp
    Added an array to draw multiple rectangles on the console :)

--- termdax.cpp
+++ termdax.cpp
@@ -1,101 +1,91 @@
-#include <sys/ioctl.h>
+#include <sys/ioctl.h>
 #include <iostream>
 #include <unistd.h>
+#include <vector>
 
-// make a array that holds the position of all rectangles and shapes
-// when you call termdax.makeRectangle() it saves the position in the array and then
-// when you call termdax.draw() it draws it in to the screen
-
-struct Termdax
+struct posList
 {
-    int windowSizeX;
-    int windowSizeY;
-    int array[]; // IN THIS ARRAY ( IT HAS TO ALLOCATE THE MEMORY DINAMICLY )
-
-    Termdax ()
-    {
-        struct winsize window;
-        ioctl(STDOUT_FILENO, TIOCGWINSZ, &window);
-        windowSizeX = window.ws_col;
-        windowSizeY = window.ws_row;
-    }
-
-    void clearScreen ()
-    {
-        system("clear");
-    }
-
-    void printWidthAndHeight () {
-        std::cout << "X: " << windowSizeX << std::endl;
-        std::cout << "Y: " << windowSizeY << std::endl;
-    }
-
-    void fillConsoleWithCaracter ( const char* simbol )
-    {
-        for ( int y = 0; y < windowSizeY; y++ ) {
-            for ( int x = 0; x < windowSizeX; x++ ) {
-                std::cout << simbol;
-            };
-        };
-    }
-
-    char getKeyPressed ( )
-    {
-        char keyPressed;
-        system("stty raw");
-        keyPressed = getchar();
-        system("stty cooked");
-        return keyPressed;
-    }
-
-    void drawRectangle ( int x_rec, int y_rec, int _width, int _height, const char* fill )
-    {
-        int width = x_rec + _width;
-        int height = y_rec + _height;
-        for ( int y = 0; y < windowSizeY; y++ ) {
-            for ( int x = 0; x < windowSizeX; x++ ) {
-                // HERE CREATE ANOTHER FOR LOOP TO LOOP EACH ONE OF THE SHAPES 
-                // THAT ARE IN THE ARRAY TO DRAW IN TO THE SCREEN
-                if ( x >= x_rec && y >= y_rec ) {
-                    if ( x <= ( width - 1 ) && y <= ( height - 1 ) ) {
-                        std::cout << fill;
-                        continue;
-                    };
-                };
-                std::cout << " ";
-
-            };
-        };
-    }
-
-    void draw ()
-    {
-        std::cout << "Hello World" << std::endl;
-    }
+    int type;
+    int cord_x;
+    int cord_y;
+    int _width;
+    int _height;
+    char _char;
+    const char* text;
 };
 
-int main() {
-    Termdax termdax;
-    int upDown = 0;
-    int leftRight = 0;
-    while ( true ) {
-        char keyPressed = termdax.getKeyPressed();
-        termdax.clearScreen();
-        if ( keyPressed == 'w' ) {
-            upDown--;
-            termdax.drawRectangle( leftRight, upDown, 2, 2, "$");
-        } else if ( keyPressed == 's' ) {
-            upDown++;
-            termdax.drawRectangle( leftRight, upDown, 2, 2, "$");
-        } else if ( keyPressed == 'a' ) {
-            leftRight--;
-            termdax.drawRectangle( leftRight, upDown, 2, 2, "$");
-        } else if ( keyPressed == 'd' ) {
-            leftRight++;
-            termdax.drawRectangle( leftRight, upDown, 2, 2, "$");
-        } else if ( keyPressed == 'q' ) {
-            exit(0);
-        };
-    };
-    return 0;
-}
+struct Termdax
+{   
+    private:
+        std::vector<posList> array;
+
+    public:
+        int windowSizeX;
+        int windowSizeY;
+
+        Termdax ()
+        {
+            struct winsize window;
+            ioctl(STDOUT_FILENO, TIOCGWINSZ, &window);
+            windowSizeX = window.ws_col;
+            windowSizeY = window.ws_row;
+        }
+
+        void clearScreen ()
+        {
+            system("clear");
+        }
+
+        void printWidthAndHeight () {
+            std::cout << "X: " << windowSizeX << std::endl;
+            std::cout << "Y: " << windowSizeY << std::endl;
+        }
+
+        void fillConsoleWithCaracter ( char simbol )
+        {
+            for ( int y = 0; y < windowSizeY; y++ ) {
+                for ( int x = 0; x < windowSizeX; x++ ) {
+                    std::cout << simbol;
+                };
+            };
+        }
+
+        char getKeyPressed ( )
+        {
+            char keyPressed;
+            system("stty raw");
+            keyPressed = getchar();
+            system("stty cooked");
+            return keyPressed;
+        }
+
+        void Draw ()
+        {
+            bool itemPrinted = false;
+            for ( int y = 0; y < windowSizeY; y++ ) {
+                for ( int x = 0; x < windowSizeX; x++ ) {
+                    for ( posList element : array ) {
+                        if ( element.type == 1 ) {
+                                int width = element.cord_x + element._width;
+                                int height = element.cord_y + element._height;
+                                if ( x >= element.cord_x && y >= element.cord_y ) {
+                                    if ( x <= ( width - 1 ) && y <= ( height - 1 ) ) {
+                                        std::cout << element._char;
+                                        itemPrinted = true;
+                                    } else {
+                                        itemPrinted = false;
+                                    };
+                                };
+                        };                    
+                    };
+                    if ( !itemPrinted ) { std::cout << " "; };
+                };
+            };
+        }
+
+        void createRectangle ( int x_rec, int y_rec, int width, int height, char fill )
+        {
+            array.push_back({1, x_rec, y_rec, width, height, fill, NULL});
+        }
+ 
+};
